16d435afa5f70b646a418c5c120ecb0e0a635d26
Shahab Yarmohammadi <yarmohammadi@gmail.com>
2025-07-05 11:07:04 -0700

    Add weekData[]

--- myDataJournal.js
+++ myDataJournal.js
@@ -1,2 +1,60 @@
-// https://github.com/shahaby/cs81-module4b-mydataexplorer
+// myDataJournal.js
 
+const weekData = [
+    {
+        day: "Monday",
+        sleepHours: 7,
+        screenTime: 5,
+        mood: "tired",
+        caffeineIntake: 2,
+        focusLevel: 6
+    },
+    {
+        day: "Tuesday",
+        sleepHours: 6,
+        screenTime: 4,
+        mood: "focused",
+        caffeineIntake: 1,
+        focusLevel: 8
+    },
+    {
+        day: "Wednesday",
+        sleepHours: 5,
+        screenTime: 8,
+        mood: "productive",
+        caffeineIntake: 3,
+        focusLevel: 7
+    },
+    {
+        day: "Thursday",
+        sleepHours: 6,
+        screenTime: 3,
+        mood: "neutral",
+        caffeineIntake: 2,
+        focusLevel: 5
+    },
+    {
+        day: "Friday",
+        sleepHours: 7,
+        screenTime: 6,
+        mood: "happy",
+        caffeineIntake: 4,
+        focusLevel: 9
+    },
+    {
+        day: "Saturday",
+        sleepHours: 8,
+        screenTime: 2,
+        mood: "relaxed",
+        caffeineIntake: 1,
+        focusLevel: 8
+    },
+    {
+        day: "Sunday",
+        sleepHours: 7,
+        screenTime: 3,
+        mood: "refreshed",
+        caffeineIntake: 2,
+        focusLevel: 7
+    }
+];
